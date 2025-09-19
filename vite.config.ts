677dd707c113ff670f67e3c81eab5e33799df8a0
victorbarbieri91 <victortavolarobarbieri@gmail.com>
2025-09-19 15:38:33 -0300

    fix: Relax TypeScript rules for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore certain warnings
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
+        warn(warning)
+      }
+    }
+  }
 })
